e1328aaecf0c435e084b17572bffa2dd5618f1e2
James Howlett <yokeshwararao@hotmail.com>
2021-07-26 10:28:01 +0530

    Initial Commit

--- src/main/java/Selenium/FindElementsConcept.java
+++ src/main/java/Selenium/FindElementsConcept.java
@@ -1,4 +1,23 @@
 package Selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
 public class FindElementsConcept {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver","./Driver/geckodriver.exe");
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://www.ebay.com/");
+        List <WebElement> LinkList= driver.findElements(By.tagName("a"));
+        //size of linked list
+        System.out.println(LinkList.size());
+        for (WebElement webElement : LinkList) {
+            System.out.println(webElement.getText());
+        }
+    }
 }
