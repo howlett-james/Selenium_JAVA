06e422cfe7a68be6474d453f6c3aa83c4166caf4
James Howlett <yokeshwararao@hotmail.com>
2021-07-26 10:56:09 +0530

    Initial Commit

--- src/main/java/Selenium/TakeScreenShot.java
+++ src/main/java/Selenium/TakeScreenShot.java
@@ -1,4 +1,21 @@
 package Selenium;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.IOException;
+
 public class TakeScreenShot {
+    public static void main(String[] args) throws IOException {
+        System.setProperty("webdriver.gecko.driver","./Driver/geckodriver.exe");
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://google.com");
+        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("C:\\Users\\yokes\\IdeaProjects\\Selenium.JAVA\\OutPut\\google.png"));
+    }
 }
