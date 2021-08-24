12e832656141e5cd81589d35f09e6b9692f2e152
James Howlett <yokeshwararao@hotmail.com>
2021-08-24 19:40:43 +0530

    Initial Commit

---
+++ src/main/java/Selenium/HeadlessBrowserTesting.java
@@ -0,0 +1,17 @@
+package Selenium;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class HeadlessBrowserTesting {
+    public static void main(String[] args) throws InterruptedException{
+        System.setProperty("webdriver.gecko.driver","./Driver/geckodriver.exe");
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+        FirefoxDriver driver = new FirefoxDriver(options);
+        driver.get("http://facebook.com");
+        System.out.println(driver.getTitle());
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
