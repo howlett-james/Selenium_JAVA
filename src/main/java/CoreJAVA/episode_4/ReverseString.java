9d3150a1407c98dcf6fad2bfdeec7d1fa1ea6a7f
James Howlett <yokeshwararao@hotmail.com>
2021-10-12 14:11:29 +0530

    Initial Commit

--- src/main/java/CoreJAVA/episode_4/ReverseString.java
+++ src/main/java/CoreJAVA/episode_4/ReverseString.java
@@ -3,11 +3,31 @@ package CoreJAVA.episode_4;
 public class ReverseString {
     public static void main(String[] args) {
         String str = "Hey There";
+        System.out.println("Input: " +str);
+
+        //Output should be erehT yeH
         char[] ch =str.toCharArray();
         StringBuilder rev= new StringBuilder();
         for(int i=ch.length-1;i>=0;i--){
             rev.append(ch[i]);
         }
-        System.out.println(rev);
+        System.out.println("Output: " +rev);
+
+        //Output should be There yeH
+        String[] str1 = str.split(" ");
+        String string1 = str1[1];
+        StringBuilder stringBuilder = new StringBuilder(str1[0]);
+        System.out.println("Output: "+ string1 +" "+ stringBuilder.reverse());
+
+        //Output should be erehT yeH
+        String[] splitted = str.split(" {2}");
+        StringBuilder temp= new StringBuilder();
+        for(int i=0;i<splitted.length;i++){
+            String split1 = splitted[i];
+            for(int j=split1.length()-1;j>=0;j--){
+                temp.append(split1.charAt(j));
+            }
+        }
+        System.out.println("Output: " +temp);
     }
 }
