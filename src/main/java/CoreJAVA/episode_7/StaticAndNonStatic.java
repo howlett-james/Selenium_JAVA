131db4dd0d678740c17371027255c96c02c142d8
James Howlett <yokeshwararao@hotmail.com>
2021-10-22 17:08:08 +0530

    Initial Commit

--- src/main/java/CoreJAVA/episode_7/StaticAndNonStatic.java
+++ src/main/java/CoreJAVA/episode_7/StaticAndNonStatic.java
@@ -2,10 +2,8 @@ package CoreJAVA.episode_7;
 
 public class StaticAndNonStatic {
     //global variables: the scope of global variables will be available across all the functions with some condition
-
     String name = "logan";
     static int age = 22;
-
     public static void main(String[] args) {
         //how to call static methods and var?
         //1. direct calling
@@ -24,7 +22,6 @@ public class StaticAndNonStatic {
         //can I access static methods by suing object reference
         obj.sum();//warning will be given
     }
-
     public void sendMail(){
         System.out.println("Send mail method");
     }
