10c19ff99095a27142deebb4b0d18089a269c9f8
Yokesh S <yokeshwara.r@cloudbankin.com>
2025-03-07 20:21:30 +0530

    Strings Intro

--- src/main/java/HackerRank_Practise/Strings/strings1.java
+++ src/main/java/HackerRank_Practise/Strings/strings1.java
@@ -3,6 +3,16 @@ package HackerRank_Practise.Strings;
 import java.util.Scanner;
 
 public class strings1 {
+    /*Given a string s and two indices, start and end, print a substring consisting of all characters in the inclusive range from start to end - 1.
+    Input Format:
+        The first line contains a single string s.
+        The second line contains two space-separated integers denoting the values of start and end.
+    Constraints:
+        1 ≤ |s| ≤ 100 (The length of s is between 1 and 100).
+        0 ≤ start < end ≤ n (The indices should be within a valid range).
+        s consists of only English alphabetic letters (a-z, A-Z).
+    Output Format:
+        Print the substring from start to end - 1.*/
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
